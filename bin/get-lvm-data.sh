57189da9ff27ae57e5e5d6f5a17680fad009a424
Andrew Gillard <andrew@lorddeath.net>
2016-01-09 21:49:26 +0000

    Split & renamed the data-fetching scripts.

--- bin/get-lvm-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Physical volume data (with segments of everything - but doesn't include top-level LVs of mirrors)
-pvs --separator '","' --segments -o pv_name,dev_size,pv_uuid,vg_name,vg_uuid,vg_attr,vg_size,vg_free,vg_extent_size,pv_fmt,pv_attr,pv_size,pv_free,pv_pe_count,pvseg_start,pvseg_size,lv_name,lv_uuid,lv_parent,seg_start_pe,seg_size_pe,seg_pe_ranges,devices,segtype,lv_attr,lv_layout,lv_role,origin,snap_percent,move_pv,stripes 1>&3
-
-# Logical volume data (including mirrored LVs, but no segment data)
-lvs --separator '","' --segments -o vg_uuid,lv_name,lv_uuid,segtype,lv_attr,lv_layout,lv_role,copy_percent,move_pv,mirror_log,stripes 1>&4
---
+++ bin/pvs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Physical volume data (with segments of everything - but doesn't include top-level LVs of mirrors)
+pvs --separator '","' --segments -o pv_name,dev_size,pv_uuid,vg_name,vg_uuid,vg_attr,vg_size,vg_free,vg_extent_size,pv_fmt,pv_attr,pv_size,pv_free,pv_pe_count,pvseg_start,pvseg_size,lv_name,lv_uuid,lv_parent,seg_start_pe,seg_size_pe,seg_pe_ranges,devices,segtype,lv_attr,lv_layout,lv_role,origin,snap_percent,move_pv,stripes
